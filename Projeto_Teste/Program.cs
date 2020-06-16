9ca20306196fa639ab7a93a1edda01dbd917e09f
Luiz Cruz <gustavo.oliv@hotmail.com>
2020-06-16 13:44:20 -0300

    Acrescentado Tenha um bom dia!

--- Projeto_Teste/Program.cs
+++ Projeto_Teste/Program.cs
@@ -5,9 +5,9 @@ namespace Projeto_Teste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
 
-            Console.WriteLine("Bom dia!");
-
             Console.WriteLine("Como você está?");
+
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }
